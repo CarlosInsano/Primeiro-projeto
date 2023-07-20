8f9e6b4c2748ff2679077215393d42ffe74045f1
Carlos Augusto R C Filho <carloinsano8888@gmail.com>
2023-07-20 10:51:39 -0300

    Ajustes

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-function entrarConta(verificarConta) {
+function entrarConta() {
 
  console.log("Criando a Funçao ao Clicar no botao")
- var elementLogin = document.getElementById("login")
+ var Login = document.getElementById("login")
+ var Pass = document.getElementsByTagName("senha")
  
- console.log(elementLogin)
- if (elementLogin.value) {
+ console.log(Login)
+ console.log(Pass)
+ if (Login.value) {
   console.log('tem valor')
-  console.log(elementLogin.value)
+  console.log(Login.value)
   console.log('mostrando o valor do Login')
-  /*se tiver valor verificar e o valor se encontra-se no bando e dados de Login*/
   
-  /* se tiver valor direcionar o cliente à pagina Home*/
-  window.location.replace("/home.html");
+  
+  /*se tiver valor direcionar o cliente à pagina Home*/
+  window.location.replace("/home.html")
+  
  } else {
   alert ('Preencha os campos')
-  
+
  }
 
 }
